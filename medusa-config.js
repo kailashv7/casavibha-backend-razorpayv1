25537b5322924cee73e3babf96ac3741a4202eaf
Uzamaki Naruto <kailashventhan123@gmail.com>
2024-04-25 13:54:28 +0530

    initial

---
+++ medusa-config.js
@@ -0,0 +1,148 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve:`medusa-payment-razorpay`,
+    options:{
+         key_id: process.env.RAZORPAY_ID,
+                key_secret: process.env.RAZORPAY_SECRET,
+                razorpay_account: process.env.RAZORPAY_ACCOUNT,                
+                automatic_expiry_period: 30, /*any value between 12minuts and 30 days expressed in minutes*/
+                manual_expiry_period: 20,
+                refund_speed: "normal", 
+                webhook_secret: process.env.RAZORPAY_SECRET,
+    }
+  },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  {
+    resolve: `medusa-fulfillment-shiprocket`,
+    options: {
+      channel_id: process.env.SHIPROCKET_CHANNEL_ID, //(required)
+      email: process.env.SHIPROCKET_EMAIL, //(required)
+      password: process.env.SHIPROCKET_PASSWORD, //(required)
+      token: "", //(required. leave empty)
+      pricing: 'calculated', //"flat_rate" or "calculated" (required)
+      length_unit: 'cm', //"mm", "cm" or "inches" (required)
+      multiple_items: 'split_shipment', //"single_shipment" or "split_shipment"(default) (required)
+      inventory_sync: false, //true or false(default) (required)
+      forward_action: 'create_order', //'create_fulfillment' or 'create_order'(default) (required)
+      return_action: 'create_order', //'create_fulfillment' or 'create_order'(default) (required)
+    }
+  },
+  {
+    resolve: `@medusajs/file-local`,
+    options: {
+      upload_dir: "uploads",
+    },
+  },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      develop: {
+        open: process.env.OPEN_BROWSER !== "false",
+      },
+    },
+  },
+  {
+    resolve: `medusa-plugin-meilisearch`,
+    options: {
+      // other options...
+      config: {
+        host: process.env.MEILISEARCH_HOST,
+        apiKey: process.env.MEILISEARCH_API_KEY,
+      },
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: ["title", "description", "variant_sku"],
+            displayedAttributes: [
+              "id",
+              "title",
+              "description",
+              "variant_sku",
+              "thumbnail",
+              "handle",
+            ],
+          },
+          primaryKey: "id",
+        },
+      },
+    },
+  },
+];
+
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  store_cors: STORE_CORS,
+  database_url: DATABASE_URL,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+  modules,
+};
